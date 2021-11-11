cbbd1df76186c3a06fed46c8617ce8df8c1682b2
yashrajkeshari <90765791+yashrajkeshari@users.noreply.github.com>
2021-11-12 00:34:01 +0530

    Add files via upload

---
+++ Remainder.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const nodemailer= require('nodemailer');
+const cron =require('node-cron');
+
+const app =express();
+
+
+
+app.listen("3000" ,function(request ,response){
+    console.log("Server Started");
+})
+
+ const mailOptions={
+     from :'onlineshare83@gmail.com',
+     to : '2003010090@ipec.org.in',
+     subject : 'happy birthday diya',
+    text:'hello haay'
+
+ };
+
+ const transporter = nodemailer.createTransport({
+     service: 'gmail',
+     auth :{
+         user : 'onlineshare83@gmail.com',
+         pass : 'yashraj@123'
+     }
+ });
+
+
+//send email
+
+
+
+ transporter.sendMail(mailOptions,(error,info)=>{
+     if(error){
+         console.log(error);
+     }
+     else{
+         console.log('email sent' + info.response);
+     }
+ });
+
